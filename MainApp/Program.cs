56a426de2e736a59d69e5b728bbff2803c27257c
LTIUser <LTIUser@INFEDL31176.lntinfotech.com>
2021-09-30 12:00:21 -0400

    Some user functionalities were added

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -148,6 +148,77 @@ namespace MainApp
 
                 }else if(firstSelect == 2){
 
+                if(firstSelect == 2){
+                    Console.WriteLine("Type 1 to search our flights");
+                    Console.WriteLine("Type 2 if to book a flight");
+                    var userSelect = Int32.Parse(Console.ReadLine());
+
+                    if(userSelect == 1){
+                        Console.WriteLine("Please enter what you'd like to search by:");
+                        var searchSelect = Console.ReadLine().ToLower();
+                    
+                        Console.WriteLine("Please enter what your keyword:");
+                        var searchKeyword = Console.ReadLine();
+
+                        switch (searchSelect)
+                        {
+                            case "departure":
+                            db.Flights.Where(f => f.DepartureLoc == searchKeyword).ToList()
+                            .ForEach(i => Console.WriteLine($"Flight number: {i.FlightNumber}, Aircraft Type: {i.AircraftType}, Departure Location: {i.DepartureLoc}, Arrival Location: {i.ArrivalLoc}"));
+                            break;
+
+                            case "arrival":
+                            db.Flights.Where(f => f.ArrivalLoc == searchKeyword).ToList()
+                            .ForEach(i => Console.WriteLine($"Flight number: {i.FlightNumber}, Aircraft Type: {i.AircraftType}, Departure Location: {i.DepartureLoc}, Arrival Location: {i.ArrivalLoc}"));
+                            break;
+
+                            case "aircraft":
+                            db.Flights.Where(f => f.AircraftType == searchKeyword).ToList()
+                            .ForEach(i => Console.WriteLine($"Flight number: {i.FlightNumber}, Aircraft Type: {i.AircraftType}, Departure Location: {i.DepartureLoc}, Arrival Location: {i.ArrivalLoc}"));
+                            break;
+
+                            default:
+                            Console.WriteLine("No results were found.");
+                            break;
+                        }
+                        
+
+                    }else if( userSelect == 2){
+
+                        ShowFlights(db);
+
+                        Console.WriteLine("Enter the flight Id:");
+                        var flighId = Int32.Parse(Console.ReadLine());
+                        db.Seats.Where(s=> s.Passenger.Name == null).Include(s => s.Section).Include(s => s.Passenger).Include(s => s.Section.Flight)
+                        .Where(s => s.Section.Flight.FlightNumber == flighId).ToList().ForEach(i => Console.WriteLine($"Seat Id: {i.SeatId}, Section: {i.Section.SectionId} ,Row: {i.Row}, Column: {i.Column}, Price: {i.price}"));
+
+                        Console.WriteLine("Enter the Seat that you would like to purchase:");
+                        var sId = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("What is the name of the passenger:");
+                        var userName = Console.ReadLine();
+                        Console.WriteLine("is the passenger a kid?");
+                        var userKidSelect = Console.ReadLine().ToLower();
+                        bool isKid = false;
+                        if(userKidSelect == "yes"){
+                            isKid = true;
+                        }
+
+                        Passenger passenger = new Passenger(){
+                            Name = userName, isKid = isKid
+                        };
+                        db.Passengers.Add(passenger);
+                        db.SaveChanges();
+
+                        db.Seats.Where(s => s.SeatId == sId).First();
+
+
+
+                    }
+                }                    
+
+                if(firstSelect == 0){
+                    continue;
+                }
                 }
             }
 
